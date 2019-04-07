eafc112696377c18e221f5d17ad84679da154bb0
leo-filgueira <leonardo_filgueira@id.uff.br>
2019-04-07 18:24:23 -0300

    Add script que importa dados no formato csv

---
+++ importa_dados.R
@@ -0,0 +1,11 @@
+# Script para leitura de dados
+
+costumers <- read.csv("Dados/olist_customers_dataset.csv", encoding = "UTF-8")
+geolocation <- read.csv("Dados/olist_geolocation_dataset.csv", encoding = "UTF-8")
+items <- read.csv("Dados/olist_order_items_dataset.csv", encoding = "UTF-8")
+payments <- read.csv("Dados/olist_order_payments_dataset.csv", encoding = "UTF-8")
+reviews <- read.csv("Dados/olist_order_reviews_dataset.csv", encoding = "UTF-8")
+orders <- read.csv("Dados/olist_orders_dataset.csv", encoding = "UTF-8")
+products <- read.csv("Dados/olist_products_dataset.csv", encoding = "UTF-8")
+sellers <- read.csv("Dados/olist_sellers_dataset.csv", encoding = "UTF-8")
+product_category <- read.csv("Dados/product_category_name_translation.csv", encoding = "UTF-8")
